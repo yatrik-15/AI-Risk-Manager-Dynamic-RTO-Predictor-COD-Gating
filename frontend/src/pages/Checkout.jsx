7aa919176de45b737900763056113b86fdaf9c34
Yatrik <yatrikprajapati15@gmail.com>
2026-08-31 22:40:49 +0530

    fix(frontend): populate full list of Indian states in checkout dropdown

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -221,9 +221,42 @@ export default function Checkout() {
                         <label className="form-label">State</label>
                         <select className="form-input form-select" name="state" onChange={handleForm} defaultValue="">
                           <option value="" disabled>Select State</option>
+                          <option value="AP">Andhra Pradesh</option>
+                          <option value="AR">Arunachal Pradesh</option>
+                          <option value="AS">Assam</option>
+                          <option value="BR">Bihar</option>
+                          <option value="CG">Chhattisgarh</option>
+                          <option value="GA">Goa</option>
+                          <option value="GJ">Gujarat</option>
+                          <option value="HR">Haryana</option>
+                          <option value="HP">Himachal Pradesh</option>
+                          <option value="JH">Jharkhand</option>
                           <option value="KA">Karnataka</option>
+                          <option value="KL">Kerala</option>
+                          <option value="MP">Madhya Pradesh</option>
                           <option value="MH">Maharashtra</option>
+                          <option value="MN">Manipur</option>
+                          <option value="ML">Meghalaya</option>
+                          <option value="MZ">Mizoram</option>
+                          <option value="NL">Nagaland</option>
+                          <option value="OD">Odisha</option>
+                          <option value="PB">Punjab</option>
+                          <option value="RJ">Rajasthan</option>
+                          <option value="SK">Sikkim</option>
+                          <option value="TN">Tamil Nadu</option>
+                          <option value="TG">Telangana</option>
+                          <option value="TR">Tripura</option>
+                          <option value="UP">Uttar Pradesh</option>
+                          <option value="UK">Uttarakhand</option>
+                          <option value="WB">West Bengal</option>
+                          <option value="AN">Andaman and Nicobar Islands</option>
+                          <option value="CH">Chandigarh</option>
+                          <option value="DN">Dadra and Nagar Haveli and Daman and Diu</option>
                           <option value="DL">Delhi</option>
+                          <option value="JK">Jammu and Kashmir</option>
+                          <option value="LA">Ladakh</option>
+                          <option value="LD">Lakshadweep</option>
+                          <option value="PY">Puducherry</option>
                         </select>
                       </div>
                     </div>
